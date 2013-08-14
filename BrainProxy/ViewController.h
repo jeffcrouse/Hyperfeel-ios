bad80c8eed5cf01d2d34660cd13608a69acfd96b
Jeff Crouse <jefftimesten@gmail.com>
2013-08-14 17:42:04 -0400

    new layout is good!

--- BrainProxy/ViewController.h
+++ BrainProxy/ViewController.h
@@ -11,10 +11,42 @@
 #import <ExternalAccessory/ExternalAccessory.h>
 #import "SRWebSocket.h"
 #import <UIKit/UIKit.h>
-#import "BrainView.h"
+
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
-@interface ViewController : UIViewController <TGAccessoryDelegate, SRWebSocketDelegate> {
+// the eSense values
+typedef struct {
+    int attention;
+    int meditation;
+} ESenseValues;
+
+// the EEG power bands
+typedef struct {
+    int delta;
+    int theta;
+    int lowAlpha;
+    int highAlpha;
+    int lowBeta;
+    int highBeta;
+    int lowGamma;
+    int highGamma;
+} EEGValues;
+
+@interface ViewController : UIViewController <TGAccessoryDelegate, SRWebSocketDelegate, UITableViewDataSource> {
+    short rawValue;
+    int rawCount;
+    int buffRawCount;
+    int blinkStrength;
+    int poorSignalValue;
+    int heartRate;
+    float respiration;
+    int heartRateAverage;
+    int heartRateAcceleration;
+    
+    ESenseValues eSenseValues;
+    EEGValues eegValues;
+    
+    //NSThread * updateThread;
 }
 
 - (void) update:(NSTimer*)t;
@@ -34,11 +66,10 @@
 - (IBAction)submitJourney:(id)sender;
 - (IBAction)resetJourney:(id)sender;
 
-@property (nonatomic, retain) IBOutlet UILabel *labelTGAccessoryStatus;
-@property (nonatomic, retain) IBOutlet UILabel *labelWebsocket;
 @property (nonatomic, retain) IBOutlet UILabel *labelWebsocketStatus;
 @property (nonatomic, retain) IBOutlet UIButton *submitButton;
 @property (nonatomic, retain) IBOutlet UIButton *resetButton;
 @property (nonatomic, retain) IBOutlet UISwitch *uiSwitch;
-@property (nonatomic, retain) IBOutlet BrainView *brainView;
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *items;
 @end
--- BrainProxy/ViewController.m
+++ BrainProxy/ViewController.m
@@ -17,11 +17,9 @@
     SRWebSocket *_webSocket;
 }
 
-@synthesize labelTGAccessoryStatus;
 @synthesize labelWebsocketStatus;
-@synthesize brainView;
 @synthesize submitButton, resetButton;
-@synthesize uiSwitch;
+@synthesize uiSwitch, tableView;
 
 - (void)viewDidLoad
 {
@@ -33,7 +31,7 @@
     [NSTimer scheduledTimerWithTimeInterval: 0.5 target: self
                                    selector: @selector(update:)
                                    userInfo: nil repeats: YES];
-    
+
     [self disableControls];
 }
 
@@ -81,12 +79,16 @@
 - (void) update:(NSTimer*)t
 {
     //NSLog(@"update");
+    /*
     if([[TGAccessoryManager sharedTGAccessoryManager] accessory] == nil){
          [labelTGAccessoryStatus setText:@"No device connected"];
     } else {
-        [labelTGAccessoryStatus setText:@"Connected!"];
+        [labelTGAccessoryStatus setText:[NSString stringWithFormat:@"connected to %@", [[[TGAccessoryManager sharedTGAccessoryManager] accessory] name]]];
     }
+    */
     
+    [[self tableView] performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
+    //[tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -98,7 +100,7 @@
 
 
 #pragma mark - Button actions
- /*
+/*
 - (IBAction)startJourney:(id)sender
 {
     NSLog(@"startJourney");
@@ -141,6 +143,8 @@
 - (IBAction)submitJourney:(id)sender
 {
     NSLog(@"submitJourney");
+    [uiSwitch setOn:NO animated:YES];
+    
     
     NSNumber* color = [NSNumber numberWithInt:[[NSUserDefaults standardUserDefaults] integerForKey:@"color"]];
     NSDictionary* data = @{@"color": color, @"route": @"submit"};
@@ -157,6 +161,18 @@
 - (IBAction)resetJourney:(id)sender
 {
     NSLog(@"resetJourney");
+    [uiSwitch setOn:NO animated:YES];
+    
+    NSNumber* color = [NSNumber numberWithInt:[[NSUserDefaults standardUserDefaults] integerForKey:@"color"]];
+    NSDictionary* data = @{@"color": color, @"route": @"reset"};
+    
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:data options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    if(_webSocket != nil) {
+        [_webSocket send: jsonString];
+    }
 }
 
 #pragma mark - SocketRocket
@@ -237,7 +253,56 @@
 //  ThinkGear-enabled device.
 - (void)dataReceived:(NSDictionary *)data {
     //NSLog(@"dataReceived");
-    [brainView onData:data];
+    //[brainView onData:data];
+    
+    
+    if([data valueForKey:@"blinkStrength"])
+        blinkStrength = [[data valueForKey:@"blinkStrength"] intValue];
+    
+    if([data valueForKey:@"raw"])
+        rawValue = [[data valueForKey:@"raw"] shortValue];
+    
+    if([data valueForKey:@"heartRate"])
+        heartRate = [[data valueForKey:@"heartRate"] intValue];
+    
+    if([data valueForKey:@"poorSignal"]) {
+        poorSignalValue = [[data valueForKey:@"poorSignal"] intValue];
+        //NSLog(@"buffered raw count: %d", buffRawCount);
+        buffRawCount = 0;
+    }
+    
+    if([data valueForKey:@"respiration"])
+        respiration = [[data valueForKey:@"respiration"] floatValue];
+    
+    if([data valueForKey:@"heartRateAverage"])
+        heartRateAverage = [[data valueForKey:@"heartRateAverage"] intValue];
+    
+    if([data valueForKey:@"heartRateAcceleration"])
+        heartRateAcceleration = [[data valueForKey:@"heartRateAcceleration"] intValue];
+    
+    if([data valueForKey:@"rawCount"])
+        rawCount = [[data valueForKey:@"rawCount"] intValue];
+    
+    
+    // check to see whether the eSense values are there. if so, we assume that
+    // all of the other data (aside from raw) is there. this is not necessarily
+    // a safe assumption.
+    if([data valueForKey:@"eSenseAttention"]){
+        
+        eSenseValues.attention =    [[data valueForKey:@"eSenseAttention"] intValue];
+        eSenseValues.meditation =   [[data valueForKey:@"eSenseMeditation"] intValue];
+        
+        eegValues.delta =       [[data valueForKey:@"eegDelta"] intValue];
+        eegValues.theta =       [[data valueForKey:@"eegTheta"] intValue];
+        eegValues.lowAlpha =    [[data valueForKey:@"eegLowAlpha"] intValue];
+        eegValues.highAlpha =   [[data valueForKey:@"eegHighAlpha"] intValue];
+        eegValues.lowBeta =     [[data valueForKey:@"eegLowBeta"] intValue];
+        eegValues.highBeta =    [[data valueForKey:@"eegHighBeta"] intValue];
+        eegValues.lowGamma =    [[data valueForKey:@"eegLowGamma"] intValue];
+        eegValues.highGamma =   [[data valueForKey:@"eegHighGamma"] intValue];
+    }
+    
+    //[[self tableView] performSelector:@selector(reloadData) withObject:nil afterDelay:1.0];
     
     if([uiSwitch isOn]) {
         
@@ -261,4 +326,205 @@
 }
 
 
+#pragma mark - Table view data source
+/*
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    switch(section) {
+        case 0: return 2;
+        case 1: return 8;
+        case 2: return 4;
+    }
+    // Return the number of rows in the section.
+    // Usually the number of items in your array (the one that holds your list)
+    return [items count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    //Where we configure the cell in each row
+    
+    NSInteger section = [indexPath indexAtPosition:0];
+    NSInteger field = [indexPath indexAtPosition:1];
+    
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell;
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    // Configure the cell... setting the text of our cell's label
+    cell.textLabel.text = [items objectAtIndex:indexPath.row];
+    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", 10]];
+    return cell;
+}
+*/
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 4;
+}
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    switch(section){
+        case 0:
+            return 2;
+        case 1:
+            return 1;
+        case 2:
+            return 7;
+        case 3:
+            return 8;
+        default:
+            return 0;
+    }
+}
+
+- (UIImage *)updateSignalStatus {
+    
+    if(poorSignalValue == 0) {
+        return [UIImage imageNamed:@"Signal_Connected"];
+    }
+    else if(poorSignalValue > 0 && poorSignalValue < 50) {
+        return [UIImage imageNamed:@"Signal_Connecting3"];
+    }
+    else if(poorSignalValue > 50 && poorSignalValue < 200) {
+        return [UIImage imageNamed:@"Signal_Connecting2"];
+    }
+    else if(poorSignalValue == 200) {
+        return [UIImage imageNamed:@"Signal_Connecting1"];
+    }
+    else {
+        return [UIImage imageNamed:@"Signal_Disconnected"];
+    }
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)_tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString * CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell;
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        // cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier] autorelease];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
+    }
+    NSInteger section = [indexPath indexAtPosition:0];
+    NSInteger field = [indexPath indexAtPosition:1];
+    
+    cell.imageView.image = nil;
+	// Configure the cell.
+    switch(section){
+        case 0:
+            switch(field){
+                case 0:
+                    [[cell textLabel] setText:@"Sensor value"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", rawValue]];
+                    break;
+                case 1:
+                    [[cell textLabel] setText:@"Raw count"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", rawCount]];
+                default:
+                    break;
+            }
+            
+            break;
+        case 1:
+            switch(field){
+                case 0:
+                    [[cell textLabel] setText:@"Poor signal"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", poorSignalValue]];
+                    [[cell imageView] setImage:[self updateSignalStatus]];
+                    break;
+                default:
+                    break;
+            }
+            
+            break;
+        case 2:
+            switch(field){
+                case 0:
+                    [[cell textLabel] setText:@"Attention"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eSenseValues.attention]];
+                    break;
+                case 1:
+                    [[cell textLabel] setText:@"Meditation"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eSenseValues.meditation]];
+                    break;
+                case 2:
+                    [[cell textLabel] setText:@"Blink strength"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", blinkStrength]];
+                    break;
+                case 3:
+                    [[cell textLabel] setText:@"Heart rate"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", heartRate]];
+                    break;
+                case 4:
+                    [[cell textLabel] setText:@"Respiration"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%f", respiration]];
+                    break;
+                case 5:
+                    [[cell textLabel] setText:@"Heart Rate Average"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", heartRateAverage]];
+                    break;
+                case 6:
+                    [[cell textLabel] setText:@"Heart Rate Acceleration"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", heartRateAcceleration]];
+                    break;
+                default:
+                    break;
+            }
+            
+            break;
+        case 3:
+            switch(field){
+                case 0:
+                    [[cell textLabel] setText:@"Delta"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.delta]];
+                    break;
+                case 1:
+                    [[cell textLabel] setText:@"Theta"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.theta]];
+                    break;
+                case 2:
+                    [[cell textLabel] setText:@"Low alpha"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.lowAlpha]];
+                    break;
+                case 3:
+                    [[cell textLabel] setText:@"High alpha"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.highAlpha]];
+                    break;
+                case 4:
+                    [[cell textLabel] setText:@"Low beta"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.lowBeta]];
+                    break;
+                case 5:
+                    [[cell textLabel] setText:@"High beta"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.highBeta]];
+                    break;
+                case 6:
+                    [[cell textLabel] setText:@"Low gamma"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.lowGamma]];
+                    break;
+                case 7:
+                    [[cell textLabel] setText:@"High gamma"];
+                    [[cell detailTextLabel] setText:[NSString stringWithFormat:@"%d", eegValues.highGamma]];
+                    break;
+                default:
+                    break;
+            }
+            
+            break;
+        default:
+            break;
+    }
+    return cell;
+}
 @end
